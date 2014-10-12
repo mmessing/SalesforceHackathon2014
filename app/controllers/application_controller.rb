338c33b4bcf7b35a47ff5e54313074f6cc2956d9
Quinn Flagg <qflagg212@gmail.com>
2014-10-12 00:35:45 -0700

    working Facebook login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_filter :get_user
+  def get_user
+    @user = User.where(id: session[:user_id]).as_json[0]
+  end
 end
