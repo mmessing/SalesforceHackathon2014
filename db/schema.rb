ae4c8291ee92656bc60f2d7633c483623542d861
Austin Kwok <austinkwok615@gmail.com>
2014-10-12 03:07:58 -0700

    image upload

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141012003523) do
+ActiveRecord::Schema.define(version: 20141012095352) do
 
   create_table "dogs", force: true do |t|
     t.string   "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20141012003523) do
     t.string   "gender"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image"
   end
 
   create_table "images", force: true do |t|
